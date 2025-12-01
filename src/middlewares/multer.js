51443332fe109b7da8f3b35f7a64c8a174c535ef
Srishti Singh <srish17816@gmail.com>
2025-12-01 15:45:02 +0530

    multer file limit set

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -2,4 +2,7 @@ import multer from "multer";
 
 const storage = multer.memoryStorage();
 
-export const upload = multer({ storage });
+export const upload = multer({
+    storage: storage,
+    limits: { fileSize: 10 * 1024 * 1024 }
+ });
