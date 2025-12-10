bb8913b08d2216f86fb17dff11a9e082a2def763
Srishti Singh <srish17816@gmail.com>
2025-12-10 12:57:50 +0530

    testing about controller only removed all others

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 import express from "express";
 import cors from "cors";
 
-import homeRoutes from "./routes/home.routes.js";
+// import homeRoutes from "./routes/home.routes.js";
 import aboutRoutes from "./routes/about.routes.js";
-import programsRoutes from "./routes/programs.routes.js";
-import eventsRoutes from "./routes/events.routes.js";
-import blogsRoutes from "./routes/blogs.routes.js";
-import teamRoutes from "./routes/team.routes.js";
-import testimonialsRoutes from "./routes/testimonials.routes.js";
+// import programsRoutes from "./routes/programs.routes.js";
+// import eventsRoutes from "./routes/events.routes.js";
+// import blogsRoutes from "./routes/blogs.routes.js";
+// import teamRoutes from "./routes/team.routes.js";
+// import testimonialsRoutes from "./routes/testimonials.routes.js";
 
 const app = express()
 
@@ -29,13 +29,13 @@ app.get("/", (req, res) => {
 
 
 // real routes
-app.use("/home", homeRoutes);
+// app.use("/home", homeRoutes);
 app.use("/about", aboutRoutes);
-app.use("/programs", programsRoutes);
-app.use("/events", eventsRoutes);
-app.use("/blogs", blogsRoutes);
-app.use("/team", teamRoutes);
-app.use("/testimonials", testimonialsRoutes);
+// app.use("/programs", programsRoutes);
+// app.use("/events", eventsRoutes);
+// app.use("/blogs", blogsRoutes);
+// app.use("/team", teamRoutes);
+// app.use("/testimonials", testimonialsRoutes);
 
 
 
